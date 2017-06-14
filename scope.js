b45d12ba50d8eaab30be61ecf776743ea97b7827
oksanaharris <oksana.harris@gmail.com>
2017-06-13 17:12:39 -1000

    Revert "finished"

--- scope.js
+++ scope.js
@@ -1,25 +1,23 @@
 // Fill in the closeLid and openLid functions to set the isCookieJarOpen variable to false or true respectively.
-
 var isCookieJarOpen = null;
 
 function closeLid() {
-  isCookieJarOpen = false;
+  /* answer here */
 }
 
 function openLid() {
-  isCookieJarOpen = true;
+  /* answer here */
 }
 
 
 // Fill in the return value for the inner function to return a string, 'Hello World' using only the variables provided
 // in both inner and outer functions
-
 function outerFunction() {
   var hello = 'Hello';
 
   function innerFunction() {
     var world = 'World';
-    return hello + ' ' + world;
+    return /* answer here */;
   }
   return innerFunction();
 }
@@ -27,15 +25,14 @@ function outerFunction() {
 
 // This is a function that takes in a 2d-array (or matrix) and returns the sum of all elements
 // It's broken due to count variables colliding into each other.  Fix it!
-
 function addMatrixElements(matrix) {
 
   var result = 0;
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var j = 0; j < matrix[i].length; j++) {
-      result = result + matrix[i][j];
+    for(var i = 0; i < matrix[i].length; i++) {
+      result = result + matrix[i][i];
     }
   }
   return result;
@@ -44,28 +41,26 @@ function addMatrixElements(matrix) {
 
 // This function is returning the wrong userObject data. It should be returning
 // Neo's information and not Morpheus'.  Fix it!
-
 function sendDataToClient() {
 
   var userObject = {
     handle: 'neo',
     authenticated: false
-  };
+  }
 
   function authenticateUser(obj, username) {
-    var userObjectOther = {
+    var userObject = {
       handle: 'morpheus',
       authenticated: false
     };
 
     if (userObject.handle === username) {
       userObject.authenticated = true;
-      return userObject;
+      return userObject
     } else {
-      return userObject;
+      return userObject
     }
   }
-
-  authenticateUser(userObject, 'neo');
-  return userObject;
+  authenticateUser(userObject, 'neo')
+  return userObject
 }
